700c1123e7630db4c92bb99b0ba42e75ddaabd2e
Kiril Ivanov <92357694+KirilIvanovv@users.noreply.github.com>
2025-07-29 18:16:56 +0300

    Add solution - CodeHub

---
+++ 8-kyu/convert-a-boolean-to-a-string/convert-a-boolean-to-a-string.cs
@@ -0,0 +1,18 @@
+public class kata
+{
+    public static string BooleanToString(bool b)
+    {
+      string a = "True";
+      string c = "False";
+      
+      if(b == true)
+      {
+        return a;
+      }
+      else
+      {
+        return c;
+      }
+      
+    }
+}
