30a2c93df532038a3411e2b19695db072704a626
Kiril Ivanov <92357694+KirilIvanovv@users.noreply.github.com>
2025-07-30 17:43:00 +0300

    Update solution - CodeHub

--- 7-kyu/complementary-dna/complementary-dna.cs
+++ 7-kyu/complementary-dna/complementary-dna.cs
@@ -1,29 +1,11 @@
 public class DnaStrand 
-  {
-      public static string MakeComplement(string dna)
-      {
-        string result = "";
-​
-        foreach(char  c in dna)
-        {
-            if(c == 'C')
-            {
-                result += 'G';
-            }
-            else if(c == 'G')
-            {
-                result += 'C';
-            }
-            else if (c == 'A')
-            {
-                result += 'T';
-            }
-            else if (c == 'T')
-            {
-                result += 'A';
-            }
-        }
-        
-        return result;
-      }
-  }
+{
+  public static string MakeComplement(string dna)
+  {            
+      return dna.Replace("A","t")
+                .Replace("T","a")
+                .Replace("G","c")
+                .Replace("C","g")
+                .ToUpper();
+    }
+}
