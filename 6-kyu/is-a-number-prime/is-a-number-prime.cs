e7f5ec30fff90abb9447fde6f6afa2167d588858
Kiril Ivanov <92357694+KirilIvanovv@users.noreply.github.com>
2025-09-08 18:07:33 +0200

    Add solution - CodeHub

---
+++ 6-kyu/is-a-number-prime/is-a-number-prime.cs
@@ -0,0 +1,18 @@
+using System;
+public static class Kata
+{
+    public static bool IsPrime(int n)
+    {
+        if (n <= 1) return false;
+        if (n == 2 || n == 3) return true;
+        if (n % 2 == 0) return false;
+​
+        int limit = (int)Math.Sqrt(n);
+        for (int i = 3; i <= limit; i += 2)
+        {
+            if (n % i == 0) return false;
+        }
+​
+        return true;
+    }
+}
