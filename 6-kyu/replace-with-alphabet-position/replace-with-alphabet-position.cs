e89e5ce577e1ec9e424fcef9482752317645742f
Kiril Ivanov <92357694+KirilIvanovv@users.noreply.github.com>
2025-10-09 23:57:07 +0300

    Update solution - CodeHub

--- 6-kyu/replace-with-alphabet-position/replace-with-alphabet-position.cs
+++ 6-kyu/replace-with-alphabet-position/replace-with-alphabet-position.cs
@@ -3,35 +3,31 @@ using System.Linq;
 using System.Text.RegularExpressions;
 using System.Collections.Generic;
 ​
-​
 public static class Kata
 {
-  public static string AlphabetPosition(string text)
-  {
-    string result = "";
-​
-    string text2 = Regex.Replace(text, @"[\d\p{P}\p{S}]", "").ToLower();
-​
-    text2 = text2.Replace(" ","");
-    char[] arr_char = text2.ToCharArray();
-    
-    Dictionary<char, int> dict = new Dictionary<char, int>();
-​
-    for (char c = 'a'; c <= 'z'; c++)
+    public static string AlphabetPosition(string text)
     {
-      dict[c] = c - 'a' + 1;
+        string result = "";
+​
+        string text2 = Regex.Replace(text, @"[\d\p{P}\p{S}]", "").ToLower();
+        text2 = text2.Replace(" ", "");
+        char[] arr_char = text2.ToCharArray();
+​
+        Dictionary<char, int> dict = new Dictionary<char, int>();
+        for (char c = 'a'; c <= 'z'; c++)
+        {
+            dict[c] = c - 'a' + 1;
+        }
+​
+        for (int i = 0; i < arr_char.Length; i++)
+        {
+            if (dict.ContainsKey(arr_char[i]))
+            {
+                int val = dict[arr_char[i]];
+                result += val.ToString() + " ";
+            }
+        }
+​
+        return result.Trim();
     }
-    
-    for (int i = 0; i < arr_char.Length; i++)
-    {
-      if (dict.ContainsKey(arr_char[i]))
-      {
-        int val = dict[arr_char[i]];
-        result += val.ToString() + " ";
-      }
-    }
-    
-    return result.Trim();
-  
-  }
 }
