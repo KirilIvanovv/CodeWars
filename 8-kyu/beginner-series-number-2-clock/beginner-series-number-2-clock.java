82377b475e820088566f9a4397b4df928484212a
Kiril Ivanov <92357694+KirilIvanovv@users.noreply.github.com>
2025-10-24 20:06:19 +0300

    Add solution - CodeHub

---
+++ 8-kyu/beginner-series-number-2-clock/beginner-series-number-2-clock.java
@@ -0,0 +1,11 @@
+public class Clock
+{
+  public static int Past(int h, int m, int s) 
+  {
+      int result = 0;
+      result += s * 1000;
+      result += m * 60 * 1000;
+      result += h * 3600 * 1000;
+      return result;
+  }
+}
