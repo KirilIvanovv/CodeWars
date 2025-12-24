524a2d846a266ef9023c3e092ef15249ddd3bdd9
Kiril Ivanov <92357694+KirilIvanovv@users.noreply.github.com>
2025-12-24 23:56:27 +0200

    Add solution - CodeHub

---
+++ 8-kyu/returning-strings/returning-strings.cs
@@ -0,0 +1,10 @@
+using System;
+​
+public static class Kata
+{
+  public static string Greet(string name)
+  {
+    string result = $"Hello, {name} how are you doing today?";
+    return result;
+  }
+}
